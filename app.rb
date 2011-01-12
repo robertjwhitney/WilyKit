6fdfee66c14163f3c7bcbbd6ff70818bc10b9ffe
Bob Whitney <BobW@BobW-bigfuel-macbook.(none)>
2011-01-12 18:07:30 -0500

    initial commit

---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+
+get '/stylesheets/:name.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass :"stylesheets/#{params[:name]}"
+end
+
+get '/' do
+  haml :index
+end
+
+get '/:view' do
+  haml :"#{params[:view]}"
+end
